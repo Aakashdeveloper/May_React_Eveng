1bfa89a182d5bb9ce6368800c5447dbff154a07b
aakashDeveloper <ahanda205@gmail.com>
2019-05-14 17:38:41 +0100

    a

--- firstapp/src/component/Header.js
+++ firstapp/src/component/Header.js
@@ -2,17 +2,29 @@ import React, {Component} from 'react';
 import './Header.css'
 
 class Header extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            title:'React App',
+            keyword:'User text Here'
+        }
+    }
 
     inputdetect(event){
-        console.log(event.target.value)
+        //console.log(event.target.value)
+        this.setState({keyword:event.target.value?event.target.value:'User Text Here'})
     }
     render(){
         return(
             <header>
                 <div className="logo"
-                onClick={() => {console.log('logo clicked')}}>Logo</div>
+                onClick={() => {console.log('logo clicked')}}>
+                {this.state.title}
+                </div>
                 <center>
-                    <input onChange={this.inputdetect}/>
+                    <input onChange={this.inputdetect.bind(this)}/>
+                    <p>{this.state.keyword}</p>
                 </center>
                 <hr/>
             </header>
--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Home from './home';
 import Header from './component/Header';
+import JSON  from './db.json';
 
+class App extends Component {
+    constructor(props){
+        super(props)
+
+        this.state={
+            news:JSON
+        }
+    }
+    render(){
+        console.log(this.state.news)
+        return(  
+            <div>
+                <Header/>
+                <h1>Hello to react</h1>
+                <h2>This is about frist app</h2>
+                <Home/>
+            </div>
+        )
+    }
+    
+}
+
+ReactDOM.render(<App/>,document.getElementById('root'))
+
+
+
+/*
 const App = () => {
     return <div>
                 <Header/>
@@ -11,5 +39,4 @@ const App = () => {
                 <Home/>
             </div>
 }
-
-ReactDOM.render(<App/>,document.getElementById('root'))
+*/
